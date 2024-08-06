54d33bf2f63b56a0ae0eb0ab9b852535e221d31f
AlfredDaimari <alfred_daimari@yahoo.com>
2024-08-06 16:23:21 +0200

    setup servers
    - ansible setup
    - scaphandre setup
    - mount file setup

---
+++ scripts/read_json.py
@@ -0,0 +1,8 @@
+# python script for reading the json output
+
+# How to read large files in python
+# read file in chunks
+# check if the pattern exists in the chunk
+# at the start opening will be 0, when opening is found find closed
+# when chunk is open find close in next chunk
+# start saving the chunk line by line into a csv
