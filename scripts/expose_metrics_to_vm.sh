e778a127877e040e79dcbfeab3dcfa0057de7c8f
AlfredDaimari <alfred_daimari@yahoo.com>
2024-10-08 18:30:35 +0200

    fix: no json

--- scripts/expose_metrics_to_vm.sh
+++ scripts/expose_metrics_to_vm.sh
@@ -2,4 +2,3 @@ vmname=cloud_controller_adaim
 vmname2=cloud0_adaim
 mount -t tmpfs "tmpfs_${vmname}" "/var/lib/libvirt/scaphandre/${vmname}" -o size=5M
 mount -t tmpfs "tmpfs_${vmname2}" "/var/lib/libvirt/scaphandre/${vmname2}" -o size=5M
-mkdir /mnt/sdc/adaim/scaphandre/json
